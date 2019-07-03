b3cff065b0629107152f5fb0e79ba798f8efc400
i_wanglu <i_wanglu@weshareholdings.com>
2019-07-03 10:59:18 +0800

    add

---
+++ src/components/board.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container"></div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+@import "./../assets/scss/partial/flex";
+.container{
+    width:360px;
+    height:360px;
+    border:2px solid white;
+    border-radius: 2px;
+}
+</style>
+
